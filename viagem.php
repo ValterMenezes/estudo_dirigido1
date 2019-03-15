c017cffb66cb95389a8bda9585728b11ecf21f4f
Valter Menezes <48133573+ValterMenezes@users.noreply.github.com>
2019-03-15 19:15:25 -0300

    Delete viagem.php

--- viagem.php
+++
@@ -1,7 +0,0 @@
-<?php
-print "\n Quantos Km deseja percorrer: ";
-$distancia = (float) fgets(STDIN);
-print "\n Média de velocidade em Km/h desejada para a viagem: ";
-$velocidade = (float) fgets (STDIN);
-$tempo = $distancia / $velocidade;
-print "\n A viagem vai demorar aproximadamente $tempo hora(s) ";
