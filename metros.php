f99adb848506c41b44ff45b348c213a8844bfd45
Valter Menezes <48133573+ValterMenezes@users.noreply.github.com>
2019-03-15 19:14:42 -0300

    Delete metros.php

--- metros.php
+++
@@ -1,6 +0,0 @@
-<?php
-print "\n Quantos M² você deseja converter em milímetro: ";
-$valor = (float) fgets(STDIN); 
-$metros = $valor;
-$valor = $valor * 1000;
-print "\n $metros M² após ser convertido equivale a $valor milímetro(s) ";
