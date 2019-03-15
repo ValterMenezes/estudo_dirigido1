28238a84b5c5643785f1e4c42fa8ead919794d0d
Valter Menezes <48133573+ValterMenezes@users.noreply.github.com>
2019-03-15 19:14:07 -0300

    Delete aluguel.php

--- aluguel.php
+++
@@ -1,7 +0,0 @@
-<?php
-print "\n Quantos quilometros foi feito com o veículo alugado: ";
-$quilometragem = (float) fgets (STDIN);
-print "\n Quantos dias o veículo foi alugado: ";
-$dias = (float) fgets (STDIN);
-$preço_a_pagar = ($dias * 60) + ($quilometragem * 0.15);
-print "\n O preço pelo aluguel do carro é: R$ $preço_a_pagar";
